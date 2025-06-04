2c25c8cbe9c6ebf4f5fba7f4c12ca6d693b1d78c
Akash <shindeakash412@gmail.com>
2025-06-04 14:23:58 +0530

    completed all the templates and functionalities like create, update, delete

--- tweet/urls.py
+++ tweet/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.index, name = 'index'),
+    path('', views.tweet_list, name = 'tweet_list'),
+    path('create/', views.create_tweet, name = 'create_tweet'),
+    path('<int:tweet_id>/edit/', views.edit_tweet, name = 'edit_tweet'),
+    path('<int:tweet_id>/delete/', views.delete_tweet, name = 'delete_tweet'),
 ]
